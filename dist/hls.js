22b31d799606a006efff22e2e8a19cbccd119fbc
nvedenyuk <n1k1ta8613@gmail.com>
2016-12-06 11:19:42 +0200

    Revert "refresh" This reverts commit c7451245a71e07f21d8fd9b09b90a2e6b71ffc8b.

--- dist/hls.js
+++ dist/hls.js
@@ -6450,11 +6450,9 @@ var LevelHelper = function () {
           newFrag.endPTS = oldFrag.endPTS;
           newFrag.duration = oldFrag.duration;
           newFrag.PTSDTSshift = oldFrag.PTSDTSshift;
+          newFrag.firstGop = oldFrag.firstGop;
           newFrag.lastGop = oldFrag.lastGop;
           PTSFrag = newFrag;
-        }
-        if (oldFrag.firstGop) {
-          newFrag.firstGop = oldFrag.firstGop;
         }
       }
 
@@ -6656,7 +6654,7 @@ var Hls = function () {
     key: 'version',
     get: function get() {
       // replaced with browserify-versionify transform
-      return '0.6.1-41';
+      return '0.6.1-39';
     }
   }, {
     key: 'Events',
